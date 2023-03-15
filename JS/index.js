16672b33cd6377a55f09bb3850f07b513262af07
Jayrell Quiambao <quiambao.jayrell@gmail.com>
2023-03-14 22:17:26 -0700

    experience section styling

--- JS/index.js
+++ JS/index.js
@@ -10,3 +10,27 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     });
 });
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tab, tab__link;
+  
+    // Get all elements with class="tab" and hide them
+    tab = document.getElementsByClassName("tab");
+    for (i = 0; i < tab.length; i++) {
+      tab[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tab__link" and remove the class "active"
+    tab__link = document.getElementsByClassName("tab__link");
+    for (i = 0; i < tab__link.length; i++) {
+      tab__link[i].className = tab__link[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
